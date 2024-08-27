8ac5f33201ca3cc7895fca0c43f5643481ee15ab
mm3176 <93787471+mm3176@users.noreply.github.com>
2024-08-27 16:25:27 +0100

    Added score settings

--- settings.py
+++ settings.py
@@ -5,15 +5,19 @@ class Settings:
         """Initialize all game settings"""
         # Score settings
         self.score = 0
+        self.score_rgb = (0, 0, 0)
+        self.score_font = 'timenewroman'
+        self.score_size = 40
+        self.score_position = (190, 20)
 
         # Screen settings
         self.screen_width = 500
-        self.screen_height = 500
+        self.screen_height = 550
         self.screen_rgb = (255, 255, 255)
 
         #Borders
         self.border_rgb = (128, 128, 128)
-        self.top_border = (0, 0, 500, 10)
-        self.bottom_border = (0, 490, 500, 10)
-        self.left_border = (0, 0, 10, 500)
-        self.right_border = (490, 0, 10, 500)
+        self.top_border = (0, 0, 550, 60)
+        self.bottom_border = (0, 540, 550, 10)
+        self.left_border = (0, 0, 10, 550)
+        self.right_border = (490, 0, 10, 550)
