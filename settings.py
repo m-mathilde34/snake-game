24cf988d1e2a9525b7c456c3aaf6460c4d68c745
mm3176 <93787471+mm3176@users.noreply.github.com>
2024-08-27 14:21:01 +0100

    Added border settings

--- settings.py
+++ settings.py
@@ -9,4 +9,11 @@ class Settings:
         # Screen settings
         self.screen_width = 500
         self.screen_height = 500
-        self.screen_rgb = (128, 128, 128)
+        self.screen_rgb = (255, 255, 255)
+
+        #Borders
+        self.border_rgb = (128, 128, 128)
+        self.top_border = (0, 0, 500, 10)
+        self.bottom_border = (0, 490, 500, 10)
+        self.left_border = (0, 0, 10, 500)
+        self.right_border = (490, 0, 10, 500)
