725d4f8da7c2bdf0a394d5a529aacc1f6e3964fe
mm3176 <93787471+mm3176@users.noreply.github.com>
2024-08-30 14:23:13 +0100

    Added snake to our game window

--- snake_game.py
+++ snake_game.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from snake import Snake
 
 class SnakeGame:
     """Class to manage game assets and behavior"""
@@ -17,6 +18,9 @@ class SnakeGame:
         # Set text font, size and text --> Score
         font = pygame.font.SysFont(self.settings.score_font, self.settings.score_size)
         self.score_text = font.render(f'Score: {self.settings.score}', 0, self.settings.score_rgb)
+
+        # Get snake
+        self.snake = Snake(self)
 
     def draw_borders(self):
         pygame.draw.rect(self.screen, self.settings.border_rgb, self.settings.left_border)
@@ -37,6 +41,10 @@ class SnakeGame:
             self.draw_borders()
             #Update score
             self.screen.blit(self.score_text, self.settings.score_position)
+            #Draw snake
+            for snake_pos in self.snake.snake[1:]:
+                self.screen.blit(self.snake.skin, snake_pos)
+            self.screen.blit(self.snake.snake_head, self.snake.snake[0])
 
             # Show the most recently drawn screen.
             pygame.display.flip()
